aba52e4b09d8b376d44363917fb9dab08a451a6e
thanhhaipham <thanhhai.150298@gmail.com>
2019-05-10 00:12:05 +0700

    food

--- healthy.py
+++ healthy.py
@@ -13,102 +13,122 @@ Myfood = db['my_food']
 #     {
 #     'image':'ga.jpg',
 #     'title':'Cơm gạo lứt, thịt gà sốt Leekumki',
-#     'price':'70000',
-#     'resources':'150gr cơm gạo lứt, 150gr thịt ức gà, 200gr rau củ ',
+#     'price':'70.000 VND',
+#     'resources':'150gr cơm gạo lứt,150gr thịt ức gà, 200gr rau củ',
 #     'calo':'388',
-#     'carb':'44,4',
-#     'protein':'36,4',
-#     'fat':'6,2'
+#     'carb':'44,4 gr',
+#     'protein':'36,4 gr',
+#     'fat':'6,2 gr'
 #     },
 #     {
 #     'image':'thitlon.jpg',
 #     'title':'Cơm gạo lứt, thịt heo sốt Teriyaki',
-#     'price':'70000',
+#     'price':'70.000 VND',
 #     'resources':'150gr cơm gạo lứt, 150gr thịt heo, 200gr rau củ',
 #     'calo':'374',
-#     'carb':'44,4',
-#     'protein':'36,4',
-#     'fat':'4,7'
+#     'carb':'44,4 gr',
+#     'protein':'36,4 gr',
+#     'fat':'4,7 gr'
 #     },
 #     {
 #     'image':'trung.jpg',
 #     'title':'Cơm gạo lứt, trứng thịt băm cốt dừa',
-#     'price':'50000',
+#     'price':'50.000 VND',
 #     'resources':'150gr cơm gạo lứt, 200gr trứng thịt băm,200gr rau củ',
 #     'calo':'525',
-#     'carb':'50,3',
-#     'protein':'24,7',
-#     'fat':'22,6'
+#     'carb':'50,3 gr',
+#     'protein':'24,7 gr',
+#     'fat':'22,6 gr'
 #     },
 #     {
 #     'image':'ca.jpg',
 #     'title':'Cơm gạo lứt, cá phi lê hấp tương gừng',
-#     'price':'60000',
+#     'price':'60.000 VND',
 #     'resources':'150gr cơm gạo lứt, 200gr cá, 200gr rau củ',
 #     'calo':'410',
-#     'carb':'44,4',
-#     'protein':'44,3',
-#     'fat':'6,3'
+#     'carb':'44,4 gr',
+#     'protein':'44,3 gr',
+#     'fat':'6,3 gr'
 #     },
 #     {
 #     'image':'thitvien.jpg',
 #     'title':'Cơm gạo lứt, thịt thăn heo viên hấp',
-#     'price':'60000',
+#     'price':'60.000 VND',
 #     'resources':'150gr cơm gạo lứt , 200gr thịt băm viên,200gr rau củ ',
 #     'calo':'488',
-#     'carb':'44,4',
-#     'protein':'43,9',
-#     'fat':'15,7'
+#     'carb':'44,4 gr',
+#     'protein':'43,9 gr',
+#     'fat':'15,7 gr'
 #     },
 #     {
 #     'image':'bo.jpg',
 #     'title':'Cơm gạo lứt, thịt bò kho vang đỏ',
-#     'price':'70000',
-#     'resources':'-	150gr cơm gạo lứt, 150gr thịt bò, 200gr rau củ',
+#     'price':'70.000 VND',
+#     'resources':'150gr cơm gạo lứt, 150gr thịt bò, 200gr rau củ',
 #     'calo':'429',
-#     'carb':'44,4',
-#     'protein':'55,9',
-#     'fat':'10,7'
+#     'carb':'44,4 gr',
+#     'protein':'55,9 gr',
+#     'fat':'10,7 gr'
 #     },
 #     {
 #     'image':'combo.jpg',
 #     'title':'Cơm trắng, thịt bò xào',
-#     'price':'50000',
+#     'price':'50.000 VND',
 #     'resources':'300gr cơm trắng,150gr thịt bò ,100gr dưa chuột',
 #     'calo':'707',
-#     'carb':'90',
-#     'protein':'58',
-#     'fat':'9,6'
+#     'carb':'90 gr',
+#     'protein':'58 gr',
+#     'fat':'9,6 gr'
 #     },
 #     {
 #     'image':'comga.jpg',
 #     'title':'Cơm trắng, thịt gà sốt Leekumki',
-#     'price':'50000',
+#     'price':'50.000 VND',
 #     'resources':'300gr cơm trắng, 200gr thịt gà , 250gr súp lơ',
 #     'calo':'690',
-#     'carb':'88,2',
-#     'protein':'52,8',
-#     'fat':'7,3'
+#     'carb':'88,2 gr',
+#     'protein':'52,8 gr',
+#     'fat':'7,3 gr'
 #     },
 #     {
 #     'image':'tom.jpg',
 #     'title':'Cơm trắng, tôm hấp',
-#     'price':'60000',
+#     'price':'60.000 VND',
 #     'resources':'300gr cơm trắng,200gr tôm, 150gr rau',
 #     'calo':'615',
-#     'carb':'91',
-#     'protein':'51,7',
-#     'fat':'2,8'
+#     'carb':'91 gr',
+#     'protein':'51,7 gr',
+#     'fat':'2,8 gr'
 #     },
 #     {
 #     'image':'comga2.jpg',
 #     'title':'Cơm trắng, thịt gà rang',
-#     'price':'60000',
-#     'resources':'300gr cơm trắng,200gr thịt gà,150gr đỗ xào',
+#     'price':'60.000 VND',
+#     'resources':'300gr cơm trắng, 200gr thịt gà, 150gr đỗ xào',
 #     'calo':'655',
-#     'carb':'91',
-#     'protein':'51,9',
-#     'fat':'6,6'
+#     'carb':'91 gr',
+#     'protein':'51,9 gr',
+#     'fat':'6,6 gr'
+#     },
+#     {
+#     'image':'comca.jpg',
+#     'title':'Cơm trắng, cá kho',
+#     'price':'50.000 VND',
+#     'resources':'300gr cơm trắng,200gr thịt bò,150gr đỗ xào',
+#     'calo':'605',
+#     'carb':'89,8 gr',
+#     'protein':'55,4 gr',
+#     'fat':'0,6 gr'
+#     },
+#     {
+#     'image':'bittet.jpg',
+#     'title':'Cơm trắng, thịt bò bít tết',
+#     'price':'80.000 VND',
+#     'resources':'300gr cơm trắng,150gr thịt bò,100gr dưa chuột',
+#     'calo':'695',
+#     'carb':'91 gr',
+#     'protein':'58 gr',
+#     'fat':'9,4 gr'
 #     }
 # ]
 
